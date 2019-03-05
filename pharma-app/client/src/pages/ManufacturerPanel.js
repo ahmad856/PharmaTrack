ab238637bb169507760efc9a703e633941f70321
Ahmad Shahid <ahmadshahid856@gmail.com>
2019-03-05 10:39:42 -0800

    updates

--- pharma-app/client/src/pages/ManufacturerPanel.js
+++ pharma-app/client/src/pages/ManufacturerPanel.js
@@ -131,15 +131,15 @@ class ManufacturerPanel extends Component {
     }
 
     componentDidMount() {
-        this.callApi()
+        this.callGetAllMedicines()
         .then(res => this.setState({ assets: this.flattenData(res.express) }))
         .catch(err => console.log(err));
         console.log("Assets");
         console.log(this.state.assets);
     }
 
-    callApi = async () => {
-        const response = await fetch('/api/hello');
+    callGetAllMedicines = async () => {
+        const response = await fetch('/get_all_medicines');
         const body = await response.json();
         if (response.status !== 200) throw Error(body.message);
         console.log(body);
