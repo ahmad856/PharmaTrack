77b0b65dfa99574445f01ba193615ef061f969ee
abdullahkamran <32141746+abdullahkamran@users.noreply.github.com>
2019-06-09 14:10:42 +0500

    Update utility.go

--- chaincode/pharma-app/utility.go
+++ chaincode/pharma-app/utility.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
+//checks if an asset exists against an assetID
 func checkAsset(APIstub shim.ChaincodeStubInterface, ID string) bool {
 	assetAsBytes, _ := APIstub.GetState(ID)
 	if assetAsBytes == nil {
@@ -12,6 +13,7 @@ func checkAsset(APIstub shim.ChaincodeStubInterface, ID string) bool {
 	return true
 }
 
+//checks if a user exists against a userID
 func checkOwner(APIstub shim.ChaincodeStubInterface, ID string) bool {
 	ownerAsBytes, _ := APIstub.GetState(ID)
 	if ownerAsBytes == nil {
